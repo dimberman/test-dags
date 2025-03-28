53e00f5aa11aa5862a26e3dd96fc512e2e170798
dimberman <danielryan2430@gmail.com>
2025-03-28 06:58:31 -0700

    first commit

---
+++ large-dag.py
@@ -0,0 +1,15 @@
+import pendulum
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+
+with DAG(
+    "large_dag",
+    schedule=None,
+    start_date=(pendulum.datetime(2024, 12, 1, tz="UTC")),
+):
+    for i in range(100):
+        BashOperator(
+            task_id=f"task_{i}",
+            bash_command="true",
+            cwd=".",
+        )
